27ba8b627bee33310523e0abf080962901c1daf6
Nir Kopler <nirkopler@gmail.com>
2022-02-04 19:58:38 +0200

    handle multipe songs

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { songLineData, songMerguiAsur } from '../../Data/songs';
+import { SongLineData, merguiSongsData, SongData } from '../../Data/songs';
 import Player from '../Player/Player';
 import Song from '../Songs/Song';
 import './Main.scss';
@@ -7,18 +7,26 @@ import './Main.scss';
 const Main: React.FC = () => {
     const [currentTime, setCurrentTime] = useState<number>(0);
     const [currentIndex, setCurrentIndex] = useState<number>(0);
+    const [currentSong, setCurrentSong] = useState<SongData | null>();
 
     useEffect(() => {
-        const currentSongLineIndex = songMerguiAsur.findIndex((l: songLineData) => l.time === currentTime);
-        if (currentSongLineIndex !== -1) {
-            setCurrentIndex(currentSongLineIndex + 1);
+        const _currentSong: SongData | undefined = merguiSongsData.find((song: SongData) => song.start <= currentTime && currentTime <= song.end);
+
+        if (_currentSong) {
+            setCurrentSong(_currentSong);
+            const _currentSongLineIndex = _currentSong.data.findIndex((l: SongLineData) => l.time === currentTime);
+            if (_currentSongLineIndex !== -1) {
+                setCurrentIndex(_currentSongLineIndex + 1);
+            }
+        } else {
+            setCurrentSong(null);
         }
     }, [currentTime]);
 
     return (
         <div className='main-component'>
             <Player currentTimeCallback={(t) => setCurrentTime(t)} />
-            <Song title='Asur' data={songMerguiAsur} currentIndex={currentIndex} />
+            {currentSong ? <Song title={currentSong.name} data={currentSong.data} currentIndex={currentIndex} /> : <h1>GREETING</h1>}
         </div>
     );
 };
