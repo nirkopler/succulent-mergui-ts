fd86c0ee9df966c3e0f9d08d533fb055b8ba5e91
Nir Kopler <nirkopler@gmail.com>
2022-02-04 22:11:15 +0200

    almost finish songs
    add translation file & data
    add dev tool for translate

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SongLineData, merguiSongsData, SongData } from '../../Data/songs';
+import { SongLineData, merguiSongsData, SongData, WordTranslationMap, WordTranslation } from '../../Data/songs';
 import Player from '../Player/Player';
 import Song from '../Songs/Song';
 import './Main.scss';
@@ -8,6 +8,7 @@ const Main: React.FC = () => {
     const [currentTime, setCurrentTime] = useState<number>(0);
     const [currentIndex, setCurrentIndex] = useState<number>(0);
     const [currentSong, setCurrentSong] = useState<SongData | null>();
+    const [dic, setDic] = useState<WordTranslationMap>({});
 
     useEffect(() => {
         const _currentSong: SongData | undefined = merguiSongsData.find((song: SongData) => song.start <= currentTime && currentTime <= song.end);
@@ -23,10 +24,43 @@ const Main: React.FC = () => {
         }
     }, [currentTime]);
 
+    const [word, setWord] = useState<string>('');
+    const [eng, setEng] = useState<string>('');
+    const [heb, setHeb] = useState<string>('');
+
+    const addWord = (e: any) => {
+        e.preventDefault();
+        setDic({ ...dic, [word]: new WordTranslation(eng, heb) });
+        setWord('');
+        setEng('');
+        setHeb('');
+    };
+
+    useEffect(() => {
+        console.log(dic);
+    }, [dic]);
+
     return (
         <div className='main-component'>
             <Player currentTimeCallback={(t) => setCurrentTime(t)} />
-            {currentSong ? <Song title={currentSong.name} data={currentSong.data} currentIndex={currentIndex} /> : <h1>GREETING</h1>}
+            <div className='dic-test'>
+                <form onSubmit={addWord}>
+                    <span>word</span>
+                    <br />
+                    <input type='text' name='word' onChange={(e) => setWord(e.target.value.toLocaleLowerCase())} value={word} />
+                    <br />
+                    <span>eng</span>
+                    <br />
+                    <input type='text' name='english' onChange={(e) => setEng(e.target.value.toLocaleLowerCase())} value={eng} />
+                    <br />
+                    <span>heb</span>
+                    <br />
+                    <input type='text' name='hebrew' onChange={(e) => setHeb(e.target.value)} value={heb} />
+                    <br />
+                    <button type='submit'>add word</button>
+                </form>
+            </div>
+            {currentSong ? <Song title={currentSong.name} data={currentSong.data} currentIndex={currentIndex} /> : <h1>🦝</h1>}
         </div>
     );
 };
