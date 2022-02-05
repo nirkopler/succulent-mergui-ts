8466049b75ee09d27b8cb123e3843d40c417e5fa
Nir Kopler <nirkopler@gmail.com>
2022-02-05 14:41:18 +0200

    fix position issue
    also add mark translated word

--- src/Components/Songs/Song.tsx
+++ src/Components/Songs/Song.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { dictionary, SongLineData, WordTranslation } from '../../Data/songs';
+import useWindowDimensions from '../../Hooks/useWindowDimensions';
 import './Song.scss';
 
 interface ISong {
@@ -57,21 +58,50 @@ interface ITranslatedWord {
     word: string;
     data: WordTranslation;
 }
+
 const TranslatedWord: React.FC<ITranslatedWord> = ({ word, data }) => {
     const [showTranslation, setShowTranslation] = useState<boolean>(false);
+    const { width: screenWidth } = useWindowDimensions();
+    const translationBubbleRef = useRef<HTMLDivElement>(null);
+    const [bubblePosition, setBubblePosition] = useState<number>(0);
+    const offset: number = 25;
+
+    useEffect(() => {
+        const bubbleSize = translationBubbleRef.current?.getBoundingClientRect();
+        if (bubbleSize) {
+            const rightX = bubbleSize.x + bubbleSize.width;
+            if (rightX >= screenWidth) {
+                const positionCalc = (rightX - screenWidth) * -1 - offset;
+                setBubblePosition(positionCalc);
+            }
+        }
+    }, [showTranslation]);
+
+    const _handleMouseOver = (e: React.MouseEvent): void => {
+        if (!showTranslation) {
+            setShowTranslation(true);
+        }
+    };
+
+    const _handleMouseLeave = (e: React.MouseEvent): void => {
+        if (showTranslation) {
+            setShowTranslation(false);
+            setBubblePosition(0);
+        }
+    };
 
     return (
-        <div
-            className='translated-word-component'
-            onMouseOver={() => setShowTranslation(true)}
-            onMouseLeave={() => setShowTranslation(false)}>
+        <div className='translated-word-component' onMouseOver={_handleMouseOver} onMouseLeave={_handleMouseLeave}>
             {showTranslation && (
-                <div className='translation-bubble'>
+                <div
+                    className='translation-bubble'
+                    ref={translationBubbleRef}
+                    style={{ position: 'absolute', top: offset, left: bubblePosition }}>
                     <span className='translation-lang'>English: {data.english}</span>
                     <span className='translation-lang'>Hebrew: {data.hebrew}</span>
                 </div>
             )}
-            <span className='word'>{word}</span>
+            <span className={`word ${showTranslation ? 'active' : ''}`}>{word}</span>
         </div>
     );
 };
---
+++ src/Hooks/useWindowDimensions.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+// get current screen size
+// use - const { height, width } = useWindowDimensions();
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+        width,
+        height,
+    };
+}
+
+export default function useWindowDimensions() {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return windowDimensions;
+}
